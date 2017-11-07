7ba8a8e539acb4935d7209d17605d98903a65a1c
fricklerhandwerk <valentin@fricklerhandwerk.de>
2017-11-07 11:54:49 +0100

    move dataset counting scripts out of the way

--- count_kosarak.py
+++ count_kosarak.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 items = set()
+records = 0
 with open('data/kosarak.dat') as file:
 	for line in file:
+		records += 1
 		items |= set(map(int, line.split(' ')))
 
+print("records:", records)
 print("items:", len(items))
