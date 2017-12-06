6185319546f91397bd71860bb4b0519d711b088a
fricklerhandwerk <valentin@fricklerhandwerk.de>
2017-12-06 12:06:27 +0100

    tidy up gui and code

--- gui.py
+++ gui.py
@@ -19,6 +19,7 @@ import os
 import pprint
 import random
 import wx
+import wx.lib.agw.floatspin as fs
 
 # The recommended way to use wx with mpl is with the WXAgg
 # backend.
@@ -30,7 +31,7 @@ from matplotlib.backends.backend_wxagg import \
     FigureCanvasWxAgg as FigCanvas, \
     NavigationToolbar2WxAgg as NavigationToolbar
 
-from math import inf
+from math import log
 
 from algorithms import *
 
@@ -92,6 +93,13 @@ class BarsFrame(wx.Frame):
         self.difference = Graph(
             self.panel, self.draw_difference,
             lower=-100, upper=100)
+        self.differenceCDF = Graph(
+            self.panel, self.draw_differenceCDF,
+            lower=-100, upper=100)
+        self.divergence = Graph(
+            self.panel, self.draw_divergence,
+            lower=80, upper=120)
+
 
         #
         # Layout with box sizers
@@ -99,9 +107,14 @@ class BarsFrame(wx.Frame):
         self.controls = self.create_controls()
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         hbox.Add(self.controls, 0, wx.ALL, border=10)
-        hbox.Add(self.create_figure(self.queries), 1, wx.EXPAND)
-        hbox.AddSpacer(10)
-        hbox.Add(self.create_figure(self.difference), 1, wx.EXPAND)
+        graphs = wx.GridSizer(rows=2, cols=2, hgap=10, vgap=0)
+        graphs.Add(self.create_figure(self.queries), 0, wx.EXPAND)
+        graphs.Add(self.create_figure(self.difference), 0, wx.EXPAND)
+        graphs.Add(self.create_figure(self.divergence), 0, wx.EXPAND)
+        graphs.Add(self.create_figure(self.differenceCDF), 0, wx.EXPAND)
+
+
+        hbox.Add(graphs, 1)
 
         self.panel.SetSizer(hbox)
         hbox.Fit(self)
@@ -135,39 +148,48 @@ class BarsFrame(wx.Frame):
             self.panel, style=wx.TE_PROCESS_ENTER | wx.ALIGN_RIGHT,
             min=-1000, max=1000, initial=100)
 
-        self.slider_label = wx.StaticText(self.panel, -1,
-            "Epsilon (1/1000): ")
+        self.label_epsilon = wx.StaticText(self.panel, -1,
+            "Epsilon (1/1000)")
         self.slider_epsilon = wx.Slider(self.panel, -1,
             minValue=1, maxValue=1000, value=100,
             style=wx.SL_AUTOTICKS | wx.SL_LABELS)
         self.slider_epsilon.SetTickFreq(1)
+        self.label_interval = wx.StaticText(self.panel, -1,
+            "Divergence interval")
+        self.slider_interval = fs.FloatSpin(self.panel, -1,
+            min_val=0, max_val=1000, value=1, digits=2,
+            agwStyle=fs.FS_RIGHT)
+        self.a_greater_b = wx.StaticText(self.panel, -1, "")
+        self.calculate_a_greater_b()
 
-        self.Bind(wx.EVT_COMMAND_SCROLL_THUMBTRACK, self.on_slider_epsilon, self.slider_epsilon)
+        self.Bind(wx.EVT_COMMAND_SCROLL_THUMBTRACK, self.on_parameter_change, self.slider_epsilon)
+        self.Bind(fs.EVT_FLOATSPIN, self.on_slider_interval, self.slider_interval)
         for widget in [self.query_a, self.query_b]:
-            self.Bind(wx.EVT_SPINCTRL, self.on_text_enter, widget)
+            self.Bind(wx.EVT_SPINCTRL, self.on_parameter_change, widget)
             self.Bind(wx.EVT_TEXT_ENTER, on_bounds_enter, widget)
 
         controls = wx.BoxSizer(wx.VERTICAL)
-        flags = wx.EXPAND | wx.ALL | wx.ALIGN_CENTER_VERTICAL
+        flags = wx.EXPAND | wx.TOP | wx.BOTTOM
         controls.Add(wx.StaticText(self.panel, -1, "Query A"))
         controls.Add(self.query_a, 0, border=3, flag=flags)
         controls.Add(wx.StaticText(self.panel, -1, "Query B"))
         controls.Add(self.query_b, 0, border=3, flag=flags)
-        controls.Add(self.slider_label, 0, flag=flags)
+        controls.Add(self.label_epsilon, 0, flag=flags)
         controls.Add(self.slider_epsilon, 0, border=3, flag=flags)
+        controls.Add(self.label_interval, 0, flag=flags)
+        controls.Add(self.slider_interval, 0, border=3, flag=flags)
+        controls.Add(self.a_greater_b, 0, border=5, flag=flags)
         return controls
 
     def draw_figure(self):
-        self.queries.plot()
-        self.difference.plot()
+        for a in [self.queries, self.difference, self.differenceCDF, self.divergence]:
+            a.plot()
 
     def draw_queries(self, event=None):
         ax = self.queries.axes
         ax.clear()
 
-        epsilon = self.slider_epsilon.GetValue() / 1000
-        a = Laplace(1/epsilon, self.query_a.GetValue())
-        b = Laplace(1/epsilon, self.query_b.GetValue())
+        a, b = self.get_distributions()
 
         lower = self.queries.lower.GetValue()
         upper = self.queries.upper.GetValue()
@@ -185,9 +207,7 @@ class BarsFrame(wx.Frame):
         ax = self.difference.axes
         ax.clear()
 
-        epsilon = self.slider_epsilon.GetValue() / 1000
-        a = Laplace(1/epsilon, self.query_a.GetValue())
-        b = Laplace(1/epsilon, self.query_b.GetValue())
+        a, b = self.get_distributions()
 
         lower = int(self.difference.lower.GetValue())
         upper = int(self.difference.upper.GetValue())
@@ -199,14 +219,62 @@ class BarsFrame(wx.Frame):
         self.difference.figure.suptitle("PDF of difference between queries")
         self.difference.draw()
 
-    def on_slider_epsilon(self, event):
+    def draw_differenceCDF(self, event=None):
+        ax = self.differenceCDF.axes
+        ax.clear()
+
+        a, b = self.get_distributions()
+
+        lower = int(self.differenceCDF.lower.GetValue())
+        upper = int(self.differenceCDF.upper.GetValue())
+        xs = np.linspace(lower, upper, self.differenceCDF.steps.GetValue())
+        ys = [a.differenceCDF(b)(x) for x in xs]
+        ax.plot(xs, ys, color="red", linewidth=2.0, linestyle="-", label="CDF Pr(A-B)")
+        ax.legend(loc='upper right')
+
+        self.differenceCDF.figure.suptitle("CDF of difference between queries")
+        self.differenceCDF.draw()
+
+    def draw_divergence(self, event=None):
+        ax = self.divergence.axes
+        ax.clear()
+
+        a, b = self.get_distributions()
+
+        interval = self.slider_interval.GetValue()
+        lower = int(self.divergence.lower.GetValue())
+        upper = int(self.divergence.upper.GetValue())
+        xs = np.linspace(lower, upper, self.divergence.steps.GetValue())
+        def divergence(x):
+            first = a.cdf(x+interval/2)-a.cdf(x-interval/2)
+            second = b.cdf(x+interval/2)-b.cdf(x-interval/2)
+            try:
+                result = log(first/second)
+            except (ZeroDivisionError, ValueError):
+                result = None
+            return result
+        ys = [divergence(x) for x in xs]
+        ax.plot(xs, ys, color="blue", linewidth=2.0, linestyle="-")
+
+        self.divergence.figure.suptitle("Divergence of queries on [x-{0},x+{0}]".format(interval/2))
+        self.divergence.draw()
+
+    def get_distributions(self):
+        epsilon = self.slider_epsilon.GetValue() / 1000
+        a = Laplace(1/epsilon, self.query_a.GetValue())
+        b = Laplace(1/epsilon, self.query_b.GetValue())
+        return a, b
+
+    def calculate_a_greater_b(self):
+        a, b = self.get_distributions()
+        self.a_greater_b.SetLabel("Pr(A > B) = {:.3f}".format(a.larger(b)))
+
+    def on_parameter_change(self, event):
         self.draw_figure()
+        self.calculate_a_greater_b()
 
-    def on_text_enter(self, event):
-        self.draw_figure()
-
-
-
+    def on_slider_interval(self, event):
+        self.draw_divergence()
 
     def on_save_plot(self, event):
         file_choices = "PNG (*.png)|*.png"
