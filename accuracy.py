a4d85c58f6a66cae1fc8d7988d0ae032d7519e91
fricklerhandwerk <valentin@fricklerhandwerk.de>
2018-01-17 15:41:27 +0100

    add another improvement on accuracy bound

--- accuracy.py
+++ accuracy.py
@@ -3,6 +3,7 @@ from gmpy2 import comb
 from gmpy2 import exp
 from gmpy2 import fsum
 from math import log
+from math import isclose
 
 from efprob.dc import *
 from algorithms import *
@@ -10,30 +11,39 @@ from algorithms import *
 gmpy2.get_context().precision = 1000
 
 
+def threshold_accuracy(b, e):
+    return 4*log(2/b)/e
+
+
 def accuracy(k, b, e):
     return 8*(log(k) + log(2/b))/e
 
 
-def reduced_accuracy(b, e):
-    return 4*log(2/b)/e
+def real_accuracy(k, b, e1, e2):
+    # this is only valid for fixed ratio of e1:e2, because we have to find b1 first.
+    # b1 = (k/b2)**(-2*e1/e2)
+    assert isclose(2*e1, e2)
+    b1 = 2/110
+    b2 = 2/11
+    return -2*log(b1)/e1
 
 k = 10
-b = 0.1
+b = 0.2
 e1 = 0.1
-e2 = 0.1
+e2 = 0.2
 e = e1 + e2
 T = 0
 a_q = accuracy(k, b, e)
-a_T = reduced_accuracy(b, e)
+a_T = threshold_accuracy(b, e)
 
 
 def threshold(x):
-    return exp(-x*e1)
+    return exp(-(x/2)*e1)
 
 
 def queries(x):
     """upper bound on probability that any of k queries is >= x"""
-    return min(1, k*exp(-x*e2/2))
+    return min(1, k*exp(-(x/2)*e2/2))
 
 
 def queries_improved(x):
@@ -54,11 +64,27 @@ def total_improved(x):
     return min(1, threshold(x) + queries_improved(x))
 
 
-print("alpha_q/2:", a_q/2)
-print("alpha_T/2:", a_T/2)
-print("b/2:", b/2)
-State.fromfun(threshold, R).plot(R(0, a_q))
-State.fromfun(queries, R).plot(R(0, a_q))
-State.fromfun(queries_improved, R).plot(R(0, a_q))
-State.fromfun(total, R).plot(R(0, a_q))
-State.fromfun(total_improved, R).plot(R(0, a_q))
+def accuracy_optimal(b):
+    a1 = (log(2*e1+e2) - log(e2*b))/e1
+    a2 = (2/e2) * (log(k) + log(2*e1+e2) - log(2*e1*b))
+    return a1 + a2
+
+
+def total_alpha_improved(x):
+    # inverse function of accuracy_optimal
+    e = 2*e1+e2
+    wow = (e)/(((exp(e1*e2*x)*(e2**e2))/((k/(2*e1))**(2*e1)))**(1/e))
+    return min(1,wow)
+
+
+MAX = 150
+# State.fromfun(threshold, R).plot(R(0, 200))
+# State.fromfun(queries, R).plot(R(0, 200))
+# State.fromfun(queries_improved, R).plot(R(0, a_q))
+print(accuracy(k,b,e))
+print(real_accuracy(k,b,e1, e2))
+print(total(real_accuracy(k,b,e1, e2)))
+print(accuracy_optimal(b))
+State.fromfun(total, R).plot(R(0, MAX))
+State.fromfun(total_alpha_improved, R).plot(R(0, MAX))
+State.fromfun(total_improved, R).plot(R(0, MAX))
