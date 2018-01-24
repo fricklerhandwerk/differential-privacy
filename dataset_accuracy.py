fix accuracy scaling

dataset_accuracy.py
@@ -10,15 +10,16 @@ def threshold(c, queries):
     return (queries[c-1] + queries[c])/2
 
 
-def epsilon(e, monotonic=True):
-    e1 = e/(1+(factor*c)**(2/3))
+def epsilon(e, c, monotonic=True):
+    f = factor(monotonic)
+    e1 = e/(1+(f*c)**(2/3))
     e2 = e - e1
     return e1, e2
 
 
-def error_rate_cdf(k, e1, e2, queries, T, c):
+def error_rate_cdf(k, e1, e2, queries, T, c, step=1):
     return zip(*uniq_xs(error_rate(a, k, e1, e2, queries, T, c)
-                        for a in np.arange(T+1)))
+                        for a in np.arange(T+1)[::step]))
 
 
 def error_rate(a, k, e1, e2, queries, T, c):
@@ -38,23 +39,31 @@ def uniq_xs(pairs_gen):
     for i, (x, y) in enumerate(pairs_gen):
         dups[x].append(i)
         pairs_list.append((x, y))
-    indices = [x for v in dups.values() for x in v[1:] if len(v) > 1]
+    indices = [x for v in dups.values() for x in v[:-1] if len(v) > 1]
     return ((x, y) for i, (x, y) in enumerate(pairs_list) if i not in indices)
+
+
+def factor(monotonic):
+    return 1 if monotonic else 2
 
 
 if __name__ == '__main__':
     queries = np.loadtxt('data/kosarak_clean.json', dtype=int)
+    step = 1
     sensitivity = 1
-    factor = 1 if monotonic else 2
-    c = 100
+    monotonic = True
+    f = factor(monotonic)
+    c = 50
     T = threshold(c, queries)
     k = len(queries)
 
     e = 0.1
-    e1, e2 = epsilon(e)
+    e1, e2 = epsilon(e, c, monotonic)
+    e1 = e1 / sensitivity
+    e2 = e2 / (sensitivity * f * c)
 
     fig, ax = plt.subplots()
-    xs, ys = error_rate_cdf(k, e1, e2, queries, T, c)
+    xs, ys = error_rate_cdf(k, e1, e2, queries, T, c, step=step)
     ax.bar(xs, ys, color="red", align='edge',
            label=r"$\mathrm{Pr}(\mathrm{SER} \leq x) \geq 1 - \beta$")
 