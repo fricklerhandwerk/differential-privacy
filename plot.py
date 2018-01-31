4dd17a3777ae3b51e528ecbef7d0bfa542ee87f2
fricklerhandwerk <valentin@fricklerhandwerk.de>
2018-01-31 21:42:01 +0100

    use counter to reduce copypaste

--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from collections import Counter
 import json
 import matplotlib.pyplot as plt
 from numpy import log
+
 
 with open('data/kosarak.json') as f:
 	kosarak = json.load(f)
@@ -13,19 +15,23 @@ with open('data/bms-pos.json') as f:
 with open('data/aol.json') as f:
 	aol = json.load(f)
 
-# The zero-th value is needed because the logscale shifts it out
-# of the graph and otherwise we would lose the highest-valued point.
-kosarak = [0] + sorted(kosarak.values())[::-1]
-bms_pos = [0] + sorted(bms_pos.values())[::-1]
-aol = [0] + sorted(aol.values())[::-1]
 N = 100000
-zipf = [0] + [N//(n+1) for n in range(N)]
+zipf = {n: N//(n+1) for n in range(N)}
+
+
+def ranks(data):
+	rank, score = zip(*((r+1, s) for r, (_, s) in
+		                enumerate(Counter(data).most_common())))
+	# The zero-th value is needed because the logscale shifts it out
+	# of the graph and otherwise we would lose the highest-valued point.
+	return (0,) + rank, (0,) + score
+
 
 fig, ax = plt.subplots()
-ax.plot(range(len(kosarak)), kosarak, 'r', label="Kosarak")
-ax.plot(range(len(bms_pos)), bms_pos, 'g', label="BMS-POS")
-ax.plot(range(len(aol)), aol, 'b', label="AOL")
-ax.plot(range(len(zipf)), zipf, 'c', label="Zipf")
+ax.plot(*ranks(kosarak), 'r', label="Kosarak")
+ax.plot(*ranks(bms_pos), 'g', label="BMS-POS")
+ax.plot(*ranks(aol), 'b', label="AOL")
+ax.plot(*ranks(zipf), 'c', label="Zipf")
 
 ax.legend(loc='upper right')
 ax.set_yscale('log')
