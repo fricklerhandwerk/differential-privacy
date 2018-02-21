918b67df652c23b1c78617f91ae4c3d3b57c57e2
fricklerhandwerk <valentin@fricklerhandwerk.de>
2018-02-21 13:01:40 +0100

    fix integration graph

--- integration.py
+++ integration.py
@@ -4,22 +4,22 @@ from matplotlib import pyplot as plt
 
 
 def lines(scale_a, loc_a, scale_b, loc_b):
-    sig_a = 2 * scale_a**2
-    sig_b = 2 * scale_b**2
-    a_x, a_y = [loc_b, loc_b], [loc_a - sig_a/2, loc_a + sig_a/2]
-    b_x, b_y = [loc_a, loc_a], [loc_b - sig_b/2, loc_b + sig_b/2]
+    var_a = 2 * scale_a**2
+    var_b = 2 * scale_b**2
+    a_x, a_y = [loc_b, loc_b], [loc_a - var_a/2, loc_a + var_a/2]
+    b_x, b_y = [loc_a, loc_a], [loc_b - var_b/2, loc_b + var_b/2]
     return a_x, a_y, b_x, b_y
 
-a_x, a_y, b_x, b_y = lines(1,2,1.5,4)
+a_x, a_y, b_x, b_y = lines(1, 2, 1.5, 4)
 
-fig, ax = plt.subplots()
-ax.plot(range(10))
+fig, ax = plt.subplots(figsize=(5, 4))
+ax.plot(range(11))
 ax.plot(a_x, a_y, 'r-')
 ax.plot(a_x, a_y, 'r_')
 ax.plot(b_y, b_x, 'r-')
 ax.plot(b_y, b_x, 'r|')
-plt.xlabel("A")
-plt.ylabel("B")
-plt.xlim(0,10)
-plt.ylim(0,10)
+plt.xlabel("dom(Y)")
+plt.ylabel("dom(X)")
+plt.xlim(0, 10)
+plt.ylim(0, 10)
 plt.show()
